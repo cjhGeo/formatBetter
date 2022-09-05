391d5cd6b790629874ae74c252ea765a29b54dbd
cjhGeo <107737553+cjhGeo@users.noreply.github.com>
2022-09-05 22:19:19 +0800

    Add files via upload
    Updated with more comments (console.log)'s

--- main.js
+++ main.js
@@ -29,31 +29,43 @@ function punctuation(text) {
         periodPresent = (text[i] == "." ? true : false);
 
         // The lengthy and ineffiecient period space requirement checker and do-er?
+
+        // This checks for the first period and starts the checking
         if (periodPresent || (counter < 3 && counter > 0)) {
+            // console.log(counter) //if you really don't understand, console.log here
             autoAdd = false;
             counter += 1;
         }
+        // This takes action based on the determined necessacity for a space behind the period
         if (counter == 3) {
+            // console.log("Current: " + text[i] + " Previous: " + prevChar + " " + counter + " What") //if you really don't understand, console.log
             text[i] = (text[i] == "." ? "." + prevChar + text[i] : (prevChar == " " ? "." + prevChar + text[i] : ". " + prevChar + text[i]));
             counter = 0;
             periodPresent = false;
             autoAdd = true;
         }
 
+        // This adds a space in front of the character which should have it
         if (punctuationPresentFP) {
             text[i] = (prevChar == " " ? text[i] : " " + text[i]);
         }
 
         //Basically just setting the next previous character
         prevChar = text[i];
+        // console.log(prevChar) //if you really don't understand, console.log
 
+        // The normal appending/stitching of the string back together character by character (with the exception of periods)
         if (autoAdd) {
             result = result + text[i];
         }
     }
+
+    //Checking whether final period is required because above period checker cuts off period if it is present at the very end
     if (!autoAdd) {
         result = result + "."
     }
+
+    //returns the hopefull less blinding paragraph
     return result;
 }
 
@@ -61,6 +73,7 @@ function punctuation(text) {
 
 // }
 
+// On button click
 formatBtn.addEventListener("click", function () {
     let result = "";
     let text = getText();
